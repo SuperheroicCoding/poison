ca6f834a89cdb490f0b7097f3991019cb1694a6d
Wolfram Sokollek <wolframsokollek@gmail.com>
2018-04-20 17:00:44 +0200

    start animation navigation end in case it was stopped

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,8 @@ import {ROUTER_LINKS} from './app-routes.token';
 import {shader} from './title-shader';
 import {Observable} from 'rxjs/Observable';
 import {HeadlineAnimationService} from './core/headline-animation.service';
+import {NavigationEnd, Router, RouterEvent} from '@angular/router';
+import {filter, tap} from 'rxjs/operators';
 
 
 @Component({
@@ -19,8 +21,15 @@ export class AppComponent {
   // we need title service to update page title.
   // the logUpdate service is used to log service worker changes.
   constructor(private titleService: TitleService,
-              @Inject(ROUTER_LINKS) public routerLinks: AppRoute[], headlineAnimations: HeadlineAnimationService) {
+              @Inject(ROUTER_LINKS) public routerLinks: AppRoute[], headlineAnimations: HeadlineAnimationService, private router: Router) {
     this.runAnimation = headlineAnimations.runAnimation;
+    this.router.events.pipe(
+      filter<RouterEvent>(value => value instanceof NavigationEnd
+      )
+    )
+      .subscribe(() => {
+        return headlineAnimations.startAnimation();
+      })
   }
 }
 
