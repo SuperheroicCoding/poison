d68dd5d3369decfe69ae44884182f9696cc767a8
Wolfram Sokollek <wolframsokollek@gmail.com>
2018-08-10 10:10:07 +0200

    improved login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,13 @@
 import {Component} from '@angular/core';
 import {AngularFireAuth} from 'angularfire2/auth';
 import {auth} from 'firebase';
+import {Observable} from 'rxjs';
+import {MatChipAvatar} from '@angular/material';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.less']
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
   constructor(public afAuth: AngularFireAuth) {
