23588cef29dcfcae24d713bb0250213b92112e72
Wolfram Sokollek <wolframsokollek@gmail.com>
2019-04-25 20:51:21 +0200

    added better service worker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,23 @@
 import {HttpClientModule} from '@angular/common/http';
 import {NgModule} from '@angular/core';
-
-import {AppComponent} from './app.component';
-import {SharedModule} from './shared/shared.module';
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {ServiceWorkerModule} from '@angular/service-worker';
+import {AkitaNgDevtools} from '@datorama/akita-ngdevtools';
+import {environment} from '../environments/environment';
+import {ROUTER_LINKS} from './app-routes.token';
+import {appRoutes, routes} from './app-routing.module';
+
+import {AppComponent} from './app.component';
+import {CoreModule} from './core/core.module';
+import {AboutComponent} from './info/about/about.component';
 import {InfoComponent} from './info/info.component';
 import {TechnologyComponent} from './info/technology/technology.component';
-import {NavItemComponent} from './nav-item/nav-item.component';
-import {CoreModule} from './core/core.module';
-import {appRoutes, routes} from './app-routing.module';
-import {AboutComponent} from './info/about/about.component';
-import {ROUTER_LINKS} from './app-routes.token';
-import {ServiceWorkerModule} from '@angular/service-worker';
-import {environment} from '../environments/environment';
-import {ServiceWorkerUpdateComponent} from './service-worker-update/service-worker-update.component';
 import {LoginModule} from './login/login.module';
 import {MainToolbarComponent} from './main-toolbar/main-toolbar.component';
-import {AkitaNgDevtools} from '@datorama/akita-ngdevtools';
+import {NavItemComponent} from './nav-item/nav-item.component';
+import {ServiceWorkerUpdateComponent} from './service-worker-update/service-worker-update.component';
+import {SharedModule} from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -38,7 +38,7 @@ import {AkitaNgDevtools} from '@datorama/akita-ngdevtools';
     appRoutes,
     BrowserAnimationsModule,
     SharedModule.forRoot(),
-    ServiceWorkerModule.register('./ngsw-worker.js', {enabled: environment.production})
+    ServiceWorkerModule.register('ngsw-worker.js', {enabled: environment.production}),
   ],
   providers: [
     {
