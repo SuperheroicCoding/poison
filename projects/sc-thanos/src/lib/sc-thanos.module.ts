10cd3122d2e45f3d2f9ad57cbd546636c60baf55
Wolfram Sokollek <wolframsokollek@gmail.com>
2019-05-09 17:07:30 +0200

    raised budget

--- projects/sc-thanos/src/lib/sc-thanos.module.ts
+++ projects/sc-thanos/src/lib/sc-thanos.module.ts
@@ -1,9 +1,12 @@
 import {ModuleWithProviders, NgModule} from '@angular/core';
-import * as html2canvas from 'html2canvas';
+import * as _html2canvas from 'html2canvas';
 import {HTML2CANVAS_INJECTION_TOKEN} from './html-2-canvas.token';
 import {ScThanosDirective} from './sc-thanos.directive';
 import {defaultScThanosConfig, SC_THANOS_OPTIONS_TOKEN, ScThanosOptions} from './sc-thanos.options';
 import {ScThanosService} from './sc-thanos.service';
+
+const html2canvas = _html2canvas;
+console.log({html2canvas});
 
 @NgModule({
   declarations: [ScThanosDirective],
