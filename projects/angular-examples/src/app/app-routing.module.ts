ef76cd0885a19c3490a5a8bd2025d79f61092ca4
Wolfram Sokollek <wolframsokollek@gmail.com>
2020-01-20 12:17:21 +0100

    updated angular material and cdk

--- projects/angular-examples/src/app/app-routing.module.ts
+++ projects/angular-examples/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import {ModuleWithProviders, NgModule} from '@angular/core';
 import {Data, Route, RouterModule} from '@angular/router';
+import {addImportToModule} from '@datorama/akita/schematics/ng-add/utils';
 import {ROUTER_LINKS} from './router-links.token';
 import {IsAuthenticatedGuard} from './core/guards/is-authenticated-guard.service';
 import {InfoComponent} from './info/info.component';
@@ -16,57 +17,57 @@ export const routes: AppRoute[] = [
   {path: 'home', component: InfoComponent, data: {linkText: 'Home'}},
   {
     path: 'fourierAnalysis',
-    loadChildren: './fourier-analysis/fourier-analysis.module#FourierAnalysisModule',
+    loadChildren: async () => import('./fourier-analysis/fourier-analysis.module').then(m => m.FourierAnalysisModule),
     data: {linkText: 'Fourier Analysis Example'}
   },
   {
     path: 'bacteriaGame',
-    loadChildren: './bacteria-game/bacteria-game.module#BacteriaGameModule',
+    loadChildren: async () => import('./bacteria-game/bacteria-game.module').then(m => m.BacteriaGameModule),
     data: {linkText: 'Bacteria Game'}
   },
   {
     path: 'shaderExamples',
-    loadChildren: './shader-examples/shader-examples.module#ShaderExamplesModule',
+    loadChildren: async () => import('./shader-examples/shader-examples.module').then(m => m.ShaderExamplesModule),
     data: {linkText: 'WebGL Shader examples with live code editor (three.js)'},
     canLoad: [IsAuthenticatedGuard]
   },
   {
     path: 'someGpuCalculations',
-    loadChildren: './some-gpu-calculation/some-gpu-calculation.module#SomeGpuCalculationModule',
+    loadChildren: async () => import('./some-gpu-calculation/some-gpu-calculation.module').then(m => m.SomeGpuCalculationModule),
     data: {linkText: 'Some Gpu Accelerated Calculations (gpu.js)'}
   },
   {
     path: 'webGl',
-    loadChildren: './web-gl/web-gl.module#WebGlModule',
+    loadChildren: async () => import('./web-gl/web-gl.module').then(m => m.WebGlModule),
     data: {linkText: 'Mandelbrot plane, lights objects (three.js)'}
   },
   {
     path: 'tensorflowExamples',
-    loadChildren: './tensorflow-examples/tensorflow-examples.module#TensorflowExamplesModule',
+    loadChildren: async () => import('./tensorflow-examples/tensorflow-examples.module').then(m => m.TensorflowExamplesModule),
     data: {linkText: 'Tensorflow examples'}
   },
   {
     path: 'neuralNetwork',
-    loadChildren: './neural-network/neural-network.module#NeuralNetworkModule',
+    loadChildren: async () => import('./neural-network/neural-network.module').then(m => m.NeuralNetworkModule),
     data: {linkText: 'Neural Networks (p5)'}
   },
   {
     path: 'reactionDiff',
-    loadChildren: './reaction-diff/reaction-diff.module#ReactionDiffModule',
+    loadChildren: async () => import('./reaction-diff/reaction-diff.module').then(m => m.ReactionDiffModule),
     data: {linkText: 'Reaction Diffusion Algorithm (gpu.js)'}
   },
   {
     path: 'poisson',
-    loadChildren: './poisson/poisson.module#PoissonModule',
+    loadChildren: async () => import('./poisson/poisson.module').then(m => m.PoissonModule),
     data: {linkText: 'Poisson Distribution Algorithm'}
   }, {
     path: 'performanceTests',
-    loadChildren: './performance-test/performance-test.module#PerformanceTestModule',
+    loadChildren: async () => import('./performance-test/performance-test.module').then(m => m.PerformanceTestModule),
     data: {linkText: 'Performance Tests'}
   },
   {
     path: 'webassemblyTests',
-    loadChildren: './wasm-test/wasm-test.module#WasmTestModule',
+    loadChildren: async () => import('./wasm-test/wasm-test.module').then(m => m.WasmTestModule),
     data: {linkText: 'Web Assembly Tests'}
   },
   {path: '**', redirectTo: '/home'}
